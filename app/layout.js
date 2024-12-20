0627e3221b21d4feed3a0f5d671220ea57ca0dbe
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-12-20 09:12:30 +0100

    added pixel

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Montserrat } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata = {
@@ -14,7 +15,38 @@ const montserrat = Montserrat({
 export default function RootLayout({ children }) {
   return (
     <html lang="uk" className={`${montserrat.className}`}>
-      <body className="bg-customLightYellow overflow-x-hidden">{children}</body>
+      <head>
+        {/* Meta Pixel Script */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '292102645600496');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
+      <body className="bg-customLightYellow overflow-x-hidden">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=292102645600496&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
