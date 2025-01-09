0472910650bdd26fe8086bacb97fe67daaee00bf
Fedir <99473719+cyanidium1@users.noreply.github.com>
2025-01-09 05:52:03 +0100

    removed cross origin policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Применяется ко всем маршрутам
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "", // Убираем заголовок
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
